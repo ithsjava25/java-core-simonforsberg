7d794e9ff3198d5fb9dac6bd5e1b2909ac4e3990
Simon Forsberg <simon.co.forsberg@gmail.com>
2025-10-15 10:20:53 +0200

    Added Product abstract class

---
+++ src/main/java/com/example/Product.java
@@ -0,0 +1,41 @@
+package com.example;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+abstract public class Product {
+
+    private final UUID id;
+    private final String name;
+    private final Category category;
+    private BigDecimal price;
+
+    protected Product(UUID id, String name, Category category, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    public UUID uuid() {
+        return id;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public Category category() {
+        return category;
+    }
+
+    public BigDecimal price() {
+        return price;
+    }
+
+    public void price(BigDecimal newPrice) {
+        this.price = newPrice;
+    }
+
+    public abstract String productDetails();
+}
